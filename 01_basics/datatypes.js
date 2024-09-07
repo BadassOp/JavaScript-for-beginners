aaf1b69fe4614dd460b09c23875724053240cd15
Shantanu Jayant <139489474+BadassOp@users.noreply.github.com>
2024-09-07 02:02:43 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node js not browser so it cannot be executed and will give error
+
+/*console.log(3
+    +
+    3); */          // not a good practice, readability should be high
+
+console.log("Shantanu Jayant");  // this is a good practice
+
+let name="Shantanu"
+let age=19
+let isLoggedIn=true
+
+// datatypes (primitive dt)
+
+/*number => range 2 to power 53
+bigint(big integer) 
+string => "" or ''
+Boolean => true/false
+null => standalone value
+undefined => value not defined
+Symbol (for uniqueness)*/
+
+
+//object
+console.log(typeof undefined);  // output is undefined
+console.log(typeof null);   // output is object
