72c24b3aad8c6dfa06863f4aeed71b8a96c436ae
Shantanu Jayant <139489474+BadassOp@users.noreply.github.com>
2024-09-17 15:08:27 +0000

    for loop

--- 05_iterations(loops)/for.js
+++ 05_iterations(loops)/for.js
@@ -1,6 +1,52 @@
 // for loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is a best number");
+    }  
+    //console.log(element);
+}
+
+// console.log(element);  //out of block scope 
+
+
+for (let i = 0; i <= 10; i++) {
+    //console.log(`outer loop value: ${i}`);
     
+    for (let j = 0; j <= 10; j++) {
+        //console.log(`inner loop value ${j} and inner loop ${i}`);
+        //console.log(i + '*' + j + ' = ' + (i*j));  //table print
+         
+    }
+}
+
+
+myArray = ["Phoenix", "Jett", "Raze", "Iso", "Neon", "Yoru"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+
+//++++++++++++++++++++++  break and continue keyword  ++++++++++++++++++++++++++++
+//break
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log("Detected 5");
+        break
+    }
+    console.log(`value of i is ${index}`); 
+}
+
+
+//continue
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log("Detected 5");
+        continue
+    }
+    console.log(`value of i is ${index}`); 
 }
