b71e776e43f21e3dcf004f1d4caeb91cc051c28a
Shantanu Jayant <139489474+BadassOp@users.noreply.github.com>
2024-12-01 13:41:17 +0000

    call and this

--- 10_oops/call.js
+++ 10_oops/call.js
@@ -11,6 +11,8 @@ function setUsername(username){
 
 function createUser(username, email, password){
     // setUsername(username)  // it will execute but not hold the value of the executed function
+
+    // With call(), an object can use a method belonging to another object.
     setUsername.call(this, username)  // .call is used to hold the reference of the executed function. Here 'this' is used as first parameter and it passes the context of the function
     this.email = email
     this.password = password
@@ -18,3 +20,20 @@ function createUser(username, email, password){
 
 const hello = new createUser("hello", "hello@gmail.com", "123")
 console.log(hello);
+
+
+// Example of CALL
+let name = {
+    firstname: "Badass",
+    lastname: "OP",
+    printfullname: function(){
+        return this.firstname + " " + this.lastname
+    }
+}
+
+let name2 = {
+    firstname: "Hello",
+    lastname: "Coder"
+}
+
+console.log(name.printfullname())
